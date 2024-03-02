01f7fbe2b03f79af8d7012571d35e8c31b8e24fa
sadcat1979 <compofixer@gmail.com>
2024-03-02 22:50:03 +0300

    скопировал шаблон

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+      //Console.Write("number = "+number+" -> ");
+      //for (int i = 2; i <= number; i=i+2) Console.Write(i+"\t");
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+        number = 0;
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
