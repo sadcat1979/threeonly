b109575bb3b7c0d209856256a74222fe4d14caef
sadcat1979 <compofixer@gmail.com>
2024-03-02 23:46:00 +0300

    набросал черновик

--- Program.cs
+++ Program.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 
-public class Answer {
-    static void PrintEvenNumbers(int number)
+public class MainBlockHomeWork {
+    static void PrintArray(string[] array)
     {
-      // Введите свое решение ниже
-      //Console.Write("number = "+number+" -> ");
-      //for (int i = 2; i <= number; i=i+2) Console.Write(i+"\t");
+        foreach (var item in array)
+        {
+            if (item.Length<4) Console.WriteLine(item);
+        }
     }
 
-
-  // Не удаляйте и не меняйте метод Main! 
     static public void Main(string[] args) {
-        int number;
-        number = 0;
-        // Не удаляйте строки ниже
-        PrintEvenNumbers(number);
+        string[] inputArray = ["1234", "1567", "-2", "computer science"];
+        
+        PrintArray(inputArray);
     }
 }
