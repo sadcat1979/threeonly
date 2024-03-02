57ee2ab4df5a40bb9a0c50f652d563c62587d69c
sadcat1979 <compofixer@gmail.com>
2024-03-02 23:57:52 +0300

    сделал печать массива и окультурил код

--- Program.cs
+++ Program.cs
@@ -5,13 +5,28 @@ public class MainBlockHomeWork {
     {
         foreach (var item in array)
         {
-            if (item.Length<4) Console.WriteLine(item);
+            Console.Write(item+" ");
         }
+        Console.WriteLine();
     }
 
     static public void Main(string[] args) {
         string[] inputArray = ["1234", "1567", "-2", "computer science"];
-        
+        //Поскольку цитата "не рекомендуется использовать коллекции",
+        //а массивы статические, поэтому создаём выходной массив таким
+        //же размером как и входной
+        string[] outputArray = new string[inputArray.Length];
+        int outputArrayIndex=0;
+        foreach (var item in inputArray)
+        {
+            if (item.Length<4) {
+                outputArray[outputArrayIndex]=item;
+                outputArrayIndex++;
+            }
+        }
+        Console.Write("Исходный массив: ");
         PrintArray(inputArray);
+        Console.Write("Результирующий массив: ");
+        PrintArray(outputArray);
     }
 }
